54440438cfb3b382d3c8e397777bb7b8d97e878f
NGUYEN DUC HAI <duc-hai.nguyen@univ-tlse3.fr>
2025-04-19 18:01:42 +0200

    Create CarteEffet.java

---
+++ ProjetPirates/src/main/java/noyau/CarteEffet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package noyau;
+
+/**
+ *
+ * @author hai
+ */
+public class CarteEffet extends Carte implements ICarte {
+    
+}
