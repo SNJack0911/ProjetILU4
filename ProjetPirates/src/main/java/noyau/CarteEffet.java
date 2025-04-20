1b70bad50bc59216832caf8d7c081cd1b71555d0
yannFort16 <145565224+yannFort16@users.noreply.github.com>
2025-04-20 13:43:44 +0200

    Replace main file

--- ProjetPirates/src/main/java/noyau/CarteEffet.java
+++ ProjetPirates/src/main/java/noyau/CarteEffet.java
@@ -4,10 +4,20 @@
  */
 package noyau;
 
+import java.util.List;
+
 /**
  *
  * @author hai
  */
 public class CarteEffet extends Carte implements ICarte {
-    
+
+    protected CarteEffet(String nom, String description, ICategorieCarte categorie, BasicCategorie zoneDepot) {
+        super(nom, description, categorie, zoneDepot);
+    }
+
+    @Override
+    public List<String> appliquerEffet(Pirate joueur, Pirate adversaire, Jeu jeu) {
+        return List.of();
+    }
 }
