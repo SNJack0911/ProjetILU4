720c8cf316b18a8a673b99abacb91c97ed03d1c2
Pop Stefan <popstefan76@gmail.com>
2025-05-21 17:01:09 +0200

    Test carte pop update

--- ProjetPirates/src/test/java/noyau/CarteTest.java
+++ ProjetPirates/src/test/java/noyau/CarteTest.java
@@ -26,7 +26,7 @@ class CarteTest {
         assertEquals(0, adversaire.getPP(), "PP adversaire incorrect");
         assertEquals(1, adversaire.getHP(), "HP adversaire incorrect");
 
-        System.out.println("✔ attackEffet");
+        System.out.println("Passed attackEffet");
     }
 
     @Test
@@ -42,7 +42,25 @@ class CarteTest {
 
         assertEquals(5, joueur.getHP(), "HP doublé car pas la nuit");
         assertEquals(2, joueur.getPP(), "PP joueur incorrect");
-        System.out.println("✔ defenseEffet");
+        System.out.println("Passed defenseEffet");
+    }
+
+    @Test
+    void testPopulariteEffet() {
+        CartePopularite carte = new CartePopularite("Popularité Test", 0, "Desc", 3, 2, false);
+
+        Jeu jeu = new Jeu();
+        Pirate joueur = new Pirate("Tom");
+        Pirate adversaire = new Pirate("Jerry");
+
+        carte.appliquerEffet(joueur, adversaire, jeu);
+
+        assertEquals(5, joueur.getHP(), "HP joueur incorrect");
+        assertEquals(3, joueur.getPP(), "PP joueur incorrect");
+        assertEquals(5, adversaire.getHP(), "HP adversaire ne doit pas changer");
+        assertEquals(0, adversaire.getPP(), "PP adversaire ne doit pas changer");
+
+        System.out.println("Passed populariteEffet");
     }
 
 }
